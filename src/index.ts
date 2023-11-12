6429b52245724b4b0433a7ab9ab8882ab943d93c
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-11 22:58:04 -0400

    GraphQL first approach implemented.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import { createHandler } from 'graphql-http/lib/use/express';
+import { schema } from './schemas';
 
 const app = express();
+app.all('/graphql', createHandler({ schema }));
 
 app.get('/', (req, res) => {
   res.send('Hello World.');
 });
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(4000, () => {
+  console.log('Listening on port 4000');
 });
---
+++ src/schemas/index.ts
@@ -0,0 +1,53 @@
+import graphql, { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
+import _ from 'lodash';
+import { User } from '../models';
+
+const users: User[] = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve: (source, args, context, info) => {
+        return _.find(users, { id: args.id });
+      }
+    },
+  }
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery
+});
+
+// /**
+//  * Construct a GraphQL schema and define the necessary resolvers.
+//  *
+//  * type Query {
+//  *   hello: String
+//  * }
+//  */
+// export const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: 'Query',
+//     fields: {
+//       hello: {
+//         type: GraphQLString,
+//         resolve: () => 'world',
+//       },
+//     },
+//   }),
+// });
